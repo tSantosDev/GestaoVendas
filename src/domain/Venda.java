46807cacdc889849dc2730a010aa6fef837fd0e7
thiago.ferreira <thiago.estudo1603@gmail.com>
2024-09-27 21:17:44 -0300

    Criação de novos métodos para atender às regras de negócio.

--- src/domain/Venda.java
+++ src/domain/Venda.java
@@ -4,10 +4,65 @@ public class Venda {
     private Cliente cliente;
     private Produto produto;
     private Pagamento pagamento;
+    private int quantidadeProdutoComprado;
 
-    public Venda(Cliente cliente, Produto produto, Pagamento pagamento) {
+    public Venda(Cliente cliente, Produto produto, Pagamento pagamento, int quantidadeProdutoComprado) {
         this.cliente = cliente;
+        if (produto.getEstoque() >= quantidadeProdutoComprado) {
+            this.produto = produto;
+        } else {
+            System.out.println("Estoque do produto " + produto.getNome() + " menor que a quantidade que deseja comprar.");
+            return;
+        }
+        this.pagamento = pagamento;
+        this.quantidadeProdutoComprado = quantidadeProdutoComprado;
+    }
+
+    public void finalizarVenda() {
+        double valorTotal = calcularTotalVenda();
+        System.out.println("O valor total da compra é de: " + valorTotal);
+        this.pagamento.processarPagamento();
+        atualizandoEstoque();
+    }
+
+    private void atualizandoEstoque() {
+        int estoqueAtualizado = this.produto.getEstoque() - this.quantidadeProdutoComprado;
+        this.produto.setEstoque(estoqueAtualizado);
+    }
+
+    private double calcularTotalVenda() {
+        return this.produto.getPreco() * this.quantidadeProdutoComprado;
+    }
+
+    public int getQuantidadeProduto() {
+        return this.quantidadeProdutoComprado;
+    }
+
+    public void setQuantidadeProduto(int quantidadeProduto) {
+        this.quantidadeProdutoComprado = quantidadeProduto;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
         this.produto = produto;
+    }
+
+    public Pagamento getPagamento() {
+        return pagamento;
+    }
+
+    public void setPagamento(Pagamento pagamento) {
         this.pagamento = pagamento;
     }
 }
